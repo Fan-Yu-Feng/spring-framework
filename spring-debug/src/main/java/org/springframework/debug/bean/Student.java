28f5e0f1c6439f846b9cf9f2e94a943fee831966
fanyufeng <fanyufeng@89holdings>
2024-04-29 18:22:34 +0800

    feat: lombok config

--- spring-debug/src/main/java/org/springframework/debug/bean/Student.java
+++ spring-debug/src/main/java/org/springframework/debug/bean/Student.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
  * @date 2024/4/20 23:32
  * @description 学生bean 提供测试使用
  * <p>
- * TODO 这个地方有问题 Spring 启动运行时，@Data 没有运行，需要排查。只能手动设置 get、set方法运行
  */
 
 
@@ -25,22 +24,6 @@ public class Student {
 	@Value("18")
 	private Integer age;
 	
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public Integer getAge() {
-		return age;
-	}
-	
-	public void setAge(Integer age) {
-		this.age = age;
-	}
 	
 	@Override
 	public String toString() {
